c81c5f3e863c632912afef45f938adced185dbd4
wfedev <wfedev@gmail.com>
2023-02-16 16:41:30 +0900

    gnb active color

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,31 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Header() {
+  const router = useRouter();
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = (e:any) => {setIsActive(current => !current);};
+  const menuData=[
+    {id: '1', ko: 'WebGL', path: '/webgl'},
+    {id: '2', ko: 'CSS',   path: '/css'},
+  ]
   return (
     <header>
-        <li><Link href='/'>Cureted webgl and css</Link></li>
-      <ul>
-        <li><Link href='/webgl'>WEBGL</Link></li>
-        <li><Link href='/css'>CSS</Link></li>
-      </ul>
+      <Link href='/'>Cureted webgl and css</Link>
+      <nav>
+        <ul>
+          {menuData.map((menu)=>{
+            return(
+              <li key={menu.id}>
+                <Link href={menu.path} style={{color: menu.path === router.pathname ? 'var(--color-4)' : 'var(--color-2)'}}>
+                  {menu.ko}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
     </header>
   )
 }
