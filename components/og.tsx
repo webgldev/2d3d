808d2f7a7be5aba5d51160e110f37fe5e2197cc8
wfedev <wfedev@gmail.com>
2023-02-17 22:20:52 +0900

    add og

---
+++ components/og.tsx
@@ -0,0 +1,68 @@
+interface Props {
+  title: string
+  subtitle: string
+  tags: string
+  publicationDate: string
+}
+
+const OG = ({ title, subtitle, tags, publicationDate }: Props) => {
+  const splitTags = tags.replaceAll(" ", "").split(",")
+
+  return (
+    <div
+      tw="flex h-[630px] w-[1200px] items-center justify-center"
+      style={{
+        background:
+          "linear-gradient(133deg, rgba(107,157,255,1) 0%, rgba(109,208,255,1) 35%, rgba(25,78,182,1) 100%)",
+      }}>
+      <div
+        tw="flex absolute h-[630px] w-[1200px]"
+        style={{
+          background: "url(https://grainy-gradients.vercel.app/noise.svg)",
+        }}></div>
+      <div tw="relative flex h-[490px] w-[1060px] flex-col bg-white p-14 py-20"
+        style={{
+          borderRadius: "10px",
+        }}
+        >
+        <h1
+          tw={`mb-10 text-5xl font-bold tracking-wide text-[#292F36]`}
+          style={{ fontFamily: "alata" }}>
+          {title}
+        </h1>
+        <h2
+          tw={`mb-4 text-3xl tracking-wide text-[#292F36]`}
+          style={{ fontFamily: "mulish" }}>
+          {subtitle}
+        </h2>
+        <div
+          tw={`flex gap-2 text-lg tracking-wide text-[#292F36]`}
+          style={{ fontFamily: "mulish" }}>
+          <span>{publicationDate}</span>
+          {splitTags && splitTags.length > 0 && (
+            <>
+              <span tw="ml-2">·</span>
+              <ul>
+                {splitTags.map((tag) => (
+                  <li key={tag} tw="ml-2">
+                    #{tag.toLowerCase()}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+        <div tw="absolute bottom-10 right-10 flex justify-end">
+          <img
+            src='https://user-images.githubusercontent.com/54713067/219627927-49e4bdc8-c1ff-47a9-afd3-4e65f795053e.svg'
+            width={85}
+            height={90}
+            alt="logo"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default OG
---
+++ pages/api/og.tsx
@@ -0,0 +1,40 @@
+import { ImageResponse } from '@vercel/og';
+import { NextRequest } from "next/server";
+import OG from "../../components/og";
+
+export const config = {runtime: 'edge',};
+const font = fetch(new URL("../../public/assets/JetBrainsMono-Bold.ttf", import.meta.url)).then(
+  (res) => res.arrayBuffer()
+)
+
+export default async function (req: NextRequest) {
+  const fontData = await font
+  const { searchParams } = new URL(req.url)
+
+  const title = searchParams.get("title") ?? "Title"
+  const subtitle = searchParams.get("subtitle") ?? "Sub title"
+  const publicationDate = searchParams.get("publicationDate") ?? "2023.02.17"
+  const tags = searchParams.get("tags") ?? "tag"
+
+  return new ImageResponse(
+    (
+      <OG
+        title={title}
+        subtitle={subtitle}
+        publicationDate={publicationDate}
+        tags={tags}
+      />
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "JetBrainsMono",
+          data: fontData,
+          style: "normal",
+        }
+      ],
+    },
+  );
+}
