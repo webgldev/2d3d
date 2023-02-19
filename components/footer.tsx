162564176bcd2d783e7757d878cb59bb790b1675
wfedev <wfedev@gmail.com>
2023-02-19 19:14:36 +0900

    edit footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
-      <Link href='https://github.com/wfedev'>from wfedev</Link>
       <Link href='https://github.com/webgldev/2d3d'>Github</Link>
     </footer>
   )
